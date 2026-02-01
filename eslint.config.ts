2b61289f4937eda969088e2b8883a5d559ff0e0d
devtectiveFlex <devtective.exe@gmail.com>
2026-02-01 21:25:59 +0300

    chore: update eslint config, add scripts for running eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,19 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    ignores: [
+      '.output/**',
+      'node_modules/**',
+    ],
+  },
+  {
+    files: [
+      "**/*.{js,mjs,cjs,ts,mts,cts}"
+    ],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
 ]);
